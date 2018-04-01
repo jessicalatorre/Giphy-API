23d3d1874eae4c63efb746b37a32d6033831e24b
Jessica La Torre <jess.latorre@gmail.com>
2018-03-31 19:41:33 -0500

    redid HTMl styling; edits js file

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,28 +1,16 @@
 //use document.ready, so buttons populate on load
 $(document).ready(function () {
     console.log('ready');
-
-    //AJAX request (asynchronous javascript plus xml) with API key with a limit of 10 results
-    //variable with query string
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=K7VHz9NppDBHaxASoEIbI1cTsz62R7vB&limit=10";
-
-    //AJAX request using jQuery and object with variable and method to send to the server
-    $.ajax({ url: queryURL, method: 'GET' })
-        //promise function to server that asks for response; changed data to response
-        .done(function (response) {
-            //console.log results
-            console.log("success got data", response);
-        });
-
+//----Global Variables Listed Below---//
     //Array of director names below. These names will need to be added to buttons on the browswer, so users can click to pull up gifs
     var filmMakerArray = ["Guillermo del Toro", "Ava DuVernay", "Pedro Almodovar", "Taika Waititi", "Jean Luc Goddard", "Spike Jonze", "Jordan Peele"];
-    //function to dynamically createg gif buttons
 
+    // var filmMaker;
+    // var results;
+    //function to dynamically createg gif buttons
     function displayButtonsFunction() {
-        //erase contents in div
-        $('#gif-button-container').empty();
+        $('#gif-buttons').empty();
         for (var i = 0; i < filmMakerArray.length; i++) {
-            // button created using jQuery value stored in gifButton variable
             var gifButton = $('<button>');
             //NOTE: Adding film-maker class, so we can use this to add new film maker when user types name into search bar
             gifButton.addClass('filmMaker');
@@ -31,24 +19,56 @@ $(document).ready(function () {
             //add data name attibute for each indexed director name when the loop iterates, so you each button is being named
             gifButton.attr('data-name', filmMakerArray[i]);
             //write the text for each indexed director name to the button each time loop iterates
-            gifButton.text(filmMakerArray)
+            gifButton.text(filmMakerArray[i]);
             //Add the buttons to the DOM under using jQuery search using div id below
-            $('#gif-button-container').append(gifButton);
+            $('#gif-buttons').append(gifButton);
+            
         }
-        console.log('does this work?');
+        
     }
+    console.log(displayButtonsFunction);
 
     function newGifButton() {
-        $('#newGifSearch').on('click', function () {
-            filmMakerArray = $('filmMaker-input').val().trim();
+        $('#newGif').on('click', function () {
+            var filmMaker = $('filmMaker-input').val().trim();
             if (filmMaker == "") {
                 return false
+            alert("please type a film maker's name");
             }
             filmMakerArray.push(filmMaker);
             displayButtonsFunction();
             return false;
         });
-
     }
+
+    console.log(newGifButton);
+
+    //This function will display gifs. will put AJAX query and promise below:
+    function displayGifs () {
+        // created a global variable called film maker. Setting the variable = to the data-name attribute using jQuery
+        var filmMaker = $(this).attr('data-name');
+        //AJAX request (asynchronous javascript plus xml) with API key with a limit of 10 results
+        //variable with query string
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + filmMaker + "api_key=K7VHz9NppDBHaxASoEIbI1cTsz62R7vB&limit=10";
+        console.log(queryURL);
+
+        //AJAX request using jQuery and object with variable and method to send to the server. Split our paired object to more easily read.
+        $.ajax({ 
+            url: queryURL, 
+            method: 'GET' 
+        })
+
+   //promise function to server that asks for response; changed data to response
+   .done(function (response) {
+    //console.log results
+    console.log("success got data", response);
+    $('#gif-button-container').empty();
+    var results = response.data;
+});
+
+}
+
+     
+
 
     });
