005a59ff4c9f29051e4f0ee9d9f02a9d0c55389d
Jessica La Torre <jess.latorre@gmail.com>
2018-04-02 12:44:38 -0500

    reformatted click handler; added AJAX request and promise

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,16 +1,13 @@
-//use document.ready, so buttons populate on load
+
 $(document).ready(function () {
     console.log('ready');
-    //----Global Variables Listed Below---//
-    //Array of director names below. These names will need to be added to buttons on the browswer, so users can click to pull up gifs
+
     var filmMakerArray = ["Guillermo del Toro", "Ava DuVernay", "Pedro Almodovar", "Taika Waititi", "Jean Luc Goddard", "Spike Jonze", "Jordan Peele"];
     console.log(filmMakerArray);
 
-    // var filmMaker;
-    // var results;
     //function to dynamically createg gif buttons
     function displayButtonsFunction() {
-        $('#gif-buttons').empty();
+        // $('#gif-buttons').empty();
         for (var i = 0; i < filmMakerArray.length; i++) {
 
             var gifButton = $('<button>');
@@ -24,66 +21,61 @@ $(document).ready(function () {
             gifButton.text(filmMakerArray[i]);
             //Add the buttons to the DOM under using jQuery search using div id below
             $('#gif-buttons').append(gifButton);
-
         }
-
     }
     console.log(displayButtonsFunction());
 
-    function newGifButton() {
-        $('#newGif').on('click', function () {
-            console.log("button clicked");
-            var filmMaker = $('user-input').val().trim();
-            if (filmMaker == "") {
-                return false
-                alert("please type a film maker's name");
-            }
+    //display gifs after film maker button clicked
+    $('button').on('click', function () {
+        //created variable to store the filmMaker attribute of data-name once the button is clicked...
+        var directorName = $(this).data('name');
+        //console log new variable to see the director name each time a button is clicked.
+        //Ensures we can search for the correct individual when we set up our AJAX Call
+        console.log(directorName);
 
-            filmMakerArray.push(filmMaker);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + directorName + "&" + "api_key=K7VHz9NppDBHaxASoEIbI1cTsz62R7vB&limit=10";
+        //console logged results. After clicking Ava DuVernay button, we see the url for her in the console.
+        console.log(queryURL);
 
-            displayButtonsFunction();
-            return false;
+        //AJAX Request
+        $.ajax({url: queryURL, method:'GET'})
+                .done(function (response){
+                console.log(response);
+                    // $('#gif-button-container').empty();
+                    // var results = response.data;
+                })
+            })
         });
-    }
-    function removeUserAddedButton() {
-        $('#removeGif').on('click', function () {
-            filmMakerArray.pop(filmMaker);
-            displayButtonsFunction();
-            return false;
-        });
-    }
 
-    console.log(newGifButton());
+//     function registerButtonClickHandler() {
+//         $('#newGif').on('click', function () {
+//             event.preventDefault();
+//             console.log("button clicked");
+//             var filmMaker = $('#user-input').val().trim();
+//             if (filmMaker == "") {
+//                 return false
+//                 alert("please type a film maker's name");
+//             }
 
-    //This function will display gifs. will put AJAX query and promise below:
-    function displayGifs() {
-        // created a global variable called film maker. Setting the variable = to the data-name attribute using jQuery
-        var filmMaker = $(this).attr('data-name');
-        //AJAX request (asynchronous javascript plus xml) with API key with a limit of 10 results
-        //variable with query string
+//             filmMakerArray.push(filmMaker);
 
-        // var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
-xhr.done(function(data) { console.log("success got data", data); });
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + filmMaker + "api_key=K7VHz9NppDBHaxASoEIbI1cTsz62R7vB&limit=10";
-        console.log(queryURL());
-
-        //AJAX request using jQuery and object with variable and method to send to the server. Split our paired object to more easily read.
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        })
-
-            //promise function to server that asks for response; changed data to response
-            .done(function (response) {
-                //console.log results
-                console.log("success got data", response);
-                $('#gif-button-container').empty();
-                var results = response.data;
-            });
-
-    }
+//             displayButtonsFunction();
+//             return false;
+//         });
+//     }
+//     function removeUserAddedButton() {
+//         $('#removeGif').on('click', function () {
+//             event.preventDefault();
+//             filmMakerArray.pop(filmMaker);
+//             displayButtonsFunction();
+//             return false;
+//         });
+//     }
 
 
+// var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
+// xhr.done(function(data) { console.log("success got data", data); });
+//var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + filmMaker + "api_key=K7VHz9NppDBHaxASoEIbI1cTsz62R7vB&limit=10";
 
 
-});
+//
